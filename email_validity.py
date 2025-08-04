1878860e5ab39bae9b3b3de0ed30da7e1be83da2
Ankit Joshi <theankitjoshi12345@gmail.com>
2025-08-03 22:27:25 -0500

    Add new files

--- email_validity.py
+++ email_validity.py
@@ -1,8 +1,8 @@
 import re
 
-email = input("Enter your email: ")
+email = input("Enter your email: ").strip()
 
-if re.search(r"^\w+@+\w+\.edu$", email):
+if re.search(r"^\w+@+\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
